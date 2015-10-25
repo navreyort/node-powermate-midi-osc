f7e9cdaa2197ec50c17e3a20777c4af6764a45e0
Akito van Troyer <nav.reyort@gmail.com>
2015-10-25 19:43:16 -0400

    Initial Commit
    Basics of getting HID input from PowerMate and converting to MIDI and
    OSC message is working. Need to make it a little more module though.
    For example, by allowing users to set address and port from command
    line arguments.

---
+++ index.js
@@ -0,0 +1,25 @@
+var midi = require('midi');
+var osc = require('node-osc');
+
+var midiOut = new midi.output();
+midiOut.openVirtualPort('PowerMate');
+var oscOut = null;new osc.Client('127.0.0.1', 8001);
+
+var PipePowerMate = require('./PipePowerMate');
+
+//Count powermates
+var powerMates = [];
+var deviceCount = PipePowerMate.countPowerMate();
+console.log(deviceCount);
+for(var i=0;i<deviceCount;i++){
+  powerMates.push(new PipePowerMate(i, midiOut, oscOut, 0, 1000));
+}
+
+process.on('SIGINT', function() {
+  for(var i=0, len = powerMates.length;i<len;i++){
+    powerMates[i].close();
+  }
+    midiOut.closePort();
+    oscOut.kill();
+    process.exit(0);
+});
